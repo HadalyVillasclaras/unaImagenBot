af791d468a567d795109ef89e296d375e4973ae6
Hadaly <hadalyvf@gmail.com>
2022-09-07 17:45:42 +0200

    rm r cached

--- bot.js
+++
@@ -1,4 +0,0 @@
-const allImagesSentences = require('./buildSentences');
-
-let randomPosition = Math.floor(Math.random() * allImagesSentences.length);
-console.log(allImagesSentences[randomPosition]);
