c22026097e793e227c582c3120768802e1ebb610
Hadaly <hadalyvf@gmail.com>
2022-09-10 14:53:17 +0200

    config

--- bot.js
+++ bot.js
@@ -1,4 +1,8 @@
+const Twit = require('twit');
+const config = require('./config.js')
 const allImagesSentences = require('./buildSentences');
 
+const twit = new Twit(config);
+console.log(config);
 let randomPosition = Math.floor(Math.random() * allImagesSentences.length);
 console.log(allImagesSentences[randomPosition]);
