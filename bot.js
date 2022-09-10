63c2a71d13f0bd93104d7fc5d96e0b4ab48550e2
Hadaly <hadalyvf@gmail.com>
2022-09-10 20:39:40 +0200

    Set interval

--- bot.js
+++ bot.js
@@ -1,21 +1,29 @@
+
+const config = require('./config.js');
 const Twit = require('twit');
-const config = require('./config.js')
+const twit = new Twit(config);
 const allImagesSentences = require('./buildSentences');
 
 let randomPosition = Math.floor(Math.random() * allImagesSentences.length);
+let tweet = { status: allImagesSentences[randomPosition] };
 
-allImagesSentences[randomPosition];
-let tweet = { 
-    status: 'Una imagen de la credibilidad'
+setInterval(tweetIt(tweet), 1000 * (86400 * daysInterval(2, 3))); // 24h = 86400secs
+
+
+function tweetIt(tweet) 
+{
+    twit.post('statuses/update', tweet, tweeted);
+    
+    function tweeted(err, data, response) 
+    {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Tweeted twit: ' + data);
+        }
+    }
 }
 
-const twit = new Twit(config);
-twit.post('statuses/update', tweet, tweeted);
-
-function tweeted(err, data, response) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('Tweeted twit: ' + data);
-    }
+function daysInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
