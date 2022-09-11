ad9609090dfe572a3756bff9d149e51cb2cfad3f
Hadaly <hadalyvf@gmail.com>
2022-09-11 12:36:07 +0200

    ig

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    consumer_key:         'pSHM4alks4doWUDDA2AXxvPFh',
-    consumer_secret:      'rVfWkksFhxzq7x0gyhCNyBWGlPTaBqpxZ8QjxhjbttUMtVkmJ0',
-    access_token:         '1236713914576052226-lShNyBNRLxuejXIgQCkp9F4dbeNNqf',
-    access_token_secret:  'xXebhc9xb1bGA5H0dqGzIFGviCeaqMFWgE9Hq1BoKeiIq'
-}
-
-// timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-// strictSSL:            true,     // optional - requires SSL certificates to be valid.
