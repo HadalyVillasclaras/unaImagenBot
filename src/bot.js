576474647866d91295bcd9cb002915e61dea13b1
Hadaly <hadalyvf@gmail.com>
2022-10-04 18:49:24 +0200

    path

--- src/bot.js
+++ src/bot.js
@@ -3,7 +3,7 @@ const Twit = require('twit');
 const twit = new Twit(config);
 const sentences = require('./buildSentences');
 
-setInterval(() => tweetIt(sentences), 3600000); 
+setInterval(() => tweetIt(sentences), 3600000/6); 
 
 function tweetIt() 
 {
